8f4d83980301dfd25bd9a0698370cb12071d123a
nevine <venasamaan@gmail.com>
2026-05-13 09:37:40 -0400

    continue working on right side bar

--- client/app/src/components/homePage/RightSideBar.tsx
+++ client/app/src/components/homePage/RightSideBar.tsx
@@ -8,11 +8,16 @@ type Props = {
 
 const RightSideBar = ({ selectedUser }: Props) => {
     return selectedUser && (
-        <div>
+        <div className={`bg-[#8185B2/10] text-white w-full relative overflow-y-scroll 
+        ${selectedUser ? "max-md:hidden" : ""}`}>
             <div className="flex flex-col items-center p-4">
                 <Image src={selectedUser.profilePic || assets.avatar_icon || ""} alt="avatar" width={80} height={80}
                     className="rounded-lg object-cover w-20 aspect-[1/1]" />
-                <h2 className="text-white text-sm  mt-4">{selectedUser.fullName}</h2>
+                <h2 className="text-white text-sm  mt-4 flex items-center gap-2">
+                    {selectedUser.fullName}
+                    <p className="w-2 h-2 bg-green-500 rounded-full"></p>
+                </h2>
+                <p className={`py-4 mx-auto text-white text-xs`}>{selectedUser.bio}</p>
             </div>
         </div>
     )
